02b8c23c59f339df3a27e0b738524466c85c9023
tonylampada <tonylampada@gmail.com>
2015-08-19 00:39:06 -0300

    SPA no django

--- djangular3/urls.py
+++ djangular3/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     # Examples:
@@ -7,4 +8,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
 ]
